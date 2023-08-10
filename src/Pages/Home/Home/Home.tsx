51c3bb612f079f902dd2956768355e970e855883
abcmehedi5 <abcmehedi5@gmail.com>
2023-08-10 19:50:42 +0600

    some change

--- src/Pages/Home/Home/Home.tsx
+++ src/Pages/Home/Home/Home.tsx
@@ -1,13 +1,17 @@
 import Contact from "../Contact/Contact";
 import Cover from "../Cover/Cover";
-
+import Faq from "../Faq/Faq";
+import Testimonial from "../Testimonial/Testimonial";
 
 const Home = () => {
-    return (
-        <div>
-            <Cover></Cover>
-        </div>
-    );
+  return (
+    <div>
+      <Cover></Cover>
+      <Testimonial></Testimonial>
+      <Faq></Faq>
+      <Contact></Contact>
+    </div>
+  );
 };
 
 export default Home;
