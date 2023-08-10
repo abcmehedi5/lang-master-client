0d7e89d727c4fb1dbeb840eac9027b975d1840fb
Mostakimw <scientist.expert@gmail.com>
2023-08-10 19:36:16 +0600

    text size fixed

--- src/Pages/Home/Home/Home.tsx
+++ src/Pages/Home/Home/Home.tsx
@@ -1,13 +1,9 @@
 import Cover from "../Cover/Cover";
-import HomepageBlog from "../HomepageBlog/HomepageBlog";
-import Lessons from "../Lessons/Lessons";
 
 const Home = () => {
   return (
     <div>
       <Cover></Cover>
-      <Lessons />
-      <HomepageBlog />
     </div>
   );
 };
