83d31c788aa9cbfecc7316bdfb24b4ceaa8cd812
abcmehedi5 <abcmehedi5@gmail.com>
2023-08-14 11:36:56 +0600

    firebase function setup

--- src/Firebase/firebase.config.ts
+++ src/Firebase/firebase.config.ts
@@ -1,9 +1,5 @@
 // Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
-// TODO: Add SDKs for Firebase products that you want to use
-// https://firebase.google.com/docs/web/setup#available-libraries
-
-// Your web app's Firebase configuration
 console.log(import.meta.env.VITE_APIKEY);
 const firebaseConfig = {
   apiKey: import.meta.env.VITE_APIKEY,
@@ -14,5 +10,4 @@ const firebaseConfig = {
 
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
-
 export default app;
--- src/Providers/AuthProvider.tsx
+++ src/Providers/AuthProvider.tsx
@@ -1,12 +1,67 @@
-import { ReactNode, createContext } from "react";
+import {
+  createUserWithEmailAndPassword,
+  onAuthStateChanged,
+  signInWithEmailAndPassword,
+  signOut,
+} from "firebase/auth";
+import { getAuth } from "firebase/auth/cordova";
+import { ReactNode, createContext, useEffect, useState } from "react";
+import app from "../Firebase/firebase.config";
 interface AuthInfo {
-  name: string;
+  user: any;
+  loginUser: any;
+  createUserEmail: any;
+  logOut: any;
+  loading: boolean;
 }
 export const AuthContext = createContext<AuthInfo | null>(null);
-
+const auth = getAuth(app);
 const AuthProvider: React.FC<{ children: ReactNode }> = ({ children }) => {
+  const [user, setUser] = useState(null);
+  const [loading, setLoading] = useState(true);
+  // create user email and password
+  const createUserEmail = (email: any, password: any) => {
+    setLoading(true);
+    return createUserWithEmailAndPassword(auth, email, password);
+  };
+
+  //   login user email and password
+  const loginUser = (email: any, password: any) => {
+    setLoading(true);
+    return signInWithEmailAndPassword(auth, email, password);
+  };
+
+  // logout user
+
+  const logOut = () => {
+    return signOut(auth);
+  };
+
+  // current user save
+
+  useEffect(() => {
+    const unsubscribe = onAuthStateChanged(auth, (currentUser: any) => {
+      if (currentUser) {
+        setUser(currentUser);
+        console.log(currentUser);
+
+        // ...
+      } else {
+        // User is signed out
+        // ...
+      }
+    });
+    return () => {
+      unsubscribe();
+    };
+  }, []);
+
   const authInfo: AuthInfo = {
-    name: "hello world",
+    user,
+    createUserEmail,
+    loginUser,
+    logOut,
+    loading,
   };
 
   return (
