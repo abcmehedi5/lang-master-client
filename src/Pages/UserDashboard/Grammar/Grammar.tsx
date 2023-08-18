1e070d1f3dc42a71e05404af4c0286e3d0f8c7f6
ArjumanJesmin <arjumanjesmin221186@gmail.com>
2023-08-18 13:11:56 +0600

    Your commit message here

--- src/Pages/UserDashboard/Grammar/Grammar.tsx
+++ src/Pages/UserDashboard/Grammar/Grammar.tsx
@@ -1,6 +1,7 @@
 import * as React from 'react'; // Import React
 import useGrammar from '../../../hooks/useGrammar';
 import GrammarCategory from './GrammarCategory';
+import { Helmet } from "react-helmet-async";
 
 const Grammar: React.FC = () => {
     const [grammar] = useGrammar();
@@ -11,6 +12,9 @@ const Grammar: React.FC = () => {
 
     return (
         <div>
+          <Helmet>
+                <title> Lang Master | Dashboard | GrammarSection </title>
+            </Helmet>
             <GrammarCategory items={tense} title={"tense"} />
             <GrammarCategory items={parts_of_speech} title={"parts_of_speech"} />
             <GrammarCategory items={genders} title={"genders" }/>
