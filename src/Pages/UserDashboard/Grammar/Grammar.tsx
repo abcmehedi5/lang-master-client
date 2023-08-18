20575cf8498f56741b449c9cb4131ff14db67fc3
Mostakimw <scientist.expert@gmail.com>
2023-08-18 18:05:16 +0600

    active link set, width changes

--- src/Pages/UserDashboard/Grammar/Grammar.tsx
+++ src/Pages/UserDashboard/Grammar/Grammar.tsx
@@ -1,25 +1,30 @@
-import * as React from 'react'; // Import React
-import useGrammar from '../../../hooks/useGrammar';
-import GrammarCategory from './GrammarCategory';
+import * as React from "react"; // Import React
+import useGrammar from "../../../hooks/useGrammar";
+import GrammarCategory from "./GrammarCategory";
 import { Helmet } from "react-helmet-async";
 
 const Grammar: React.FC = () => {
-    const [grammar] = useGrammar();
-    const tense = grammar.filter((item: { category: string; }) => item.category === 'tense');
-    const parts_of_speech = grammar.filter((item: { category: string; }) => item.category === 'parts_of_speech');
-    const genders = grammar.filter((item: { category: string; }) => item.category === 'genders');
+  const [grammar] = useGrammar();
+  const tense = grammar.filter(
+    (item: { category: string }) => item.category === "tense"
+  );
+  const parts_of_speech = grammar.filter(
+    (item: { category: string }) => item.category === "parts_of_speech"
+  );
+  const genders = grammar.filter(
+    (item: { category: string }) => item.category === "genders"
+  );
 
-
-    return (
-        <div>
-            <Helmet>
-                <title> Lang Master | Dashboard | GrammarSection </title>
-            </Helmet>
-            <GrammarCategory items={tense} title={"tense"} />
-            <GrammarCategory items={parts_of_speech} title={"parts_of_speech"} />
-            <GrammarCategory items={genders} title={"genders"} />
-        </div>
-    );
+  return (
+    <div className="w-9/12 mx-auto">
+      <Helmet>
+        <title> Lang Master | Dashboard | GrammarSection </title>
+      </Helmet>
+      <GrammarCategory items={tense} title={"tense"} />
+      <GrammarCategory items={parts_of_speech} title={"parts_of_speech"} />
+      <GrammarCategory items={genders} title={"genders"} />
+    </div>
+  );
 };
 
-export default Grammar; 
+export default Grammar;
