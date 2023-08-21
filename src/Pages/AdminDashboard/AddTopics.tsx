9fe7e2ffc62568d7492375f6bbc965cdbdb71f29
Akas_Datta <012akasdatta@gmail.com>
2023-08-22 02:30:06 +0600

    addtopics component for admindashboard done

--- src/Pages/AdminDashboard/AddTopics.tsx
+++ src/Pages/AdminDashboard/AddTopics.tsx
@@ -1,78 +1,79 @@
-import React, { useState } from "react";
+import { useState } from "react";
 import { Helmet } from "react-helmet-async";
 
 const AddTopics: React.FC = () => {
-  const [question, setQuestion] = useState<string>("");
-  const [options, setOptions] = useState<string[]>(["", "", "", ""]);
-  const [correctAnswer, setCorrectAnswer] = useState<string>("");
+    const [lession, setLession] = useState<string>("");
+    const [point, setPoint] = useState<string>(""); 
+    const [percent, setPercent] = useState<string>("");
 
   const handleSubmit = (e: React.FormEvent<HTMLFormElement>) => {
     e.preventDefault();
-    const quize = {
-      question: question,
-      options: options,
-      correctAnswer: correctAnswer,
-    };
-    // Log form input values to console
-    console.log(quize);
+   
   };
 
   return (
     <>
       <Helmet>
-        <title> Add Quiz | Lang Master </title>
+        <title> Add Topics | Lang Master </title>
       </Helmet>
-      hhhi
-      <div className="p-4 lg:w-11/12">
+      <div className="p-4 py-8 lg:w-11/12">
         <form onSubmit={handleSubmit} className="space-y-4">
-          <h1 className="text-2xl font-semibold">Add Question</h1>
+          <h1 className="text-2xl font-semibold">টপিকগুলো এড করুনঃ </h1>
           <div>
-            <label className="block text-sm font-medium">Question:</label>
+            <label className="block text-sm font-medium">আপনার টপিকের নামঃ</label>
             <input
               type="text"
-              value={question}
-              onChange={(e) => setQuestion(e.target.value)}
               className="w-full border rounded-md p-2 mt-2 input input-bordered"
-              placeholder="What's your question?"
+              placeholder="আপনার টপিকের নাম কী?"
               required
             />
           </div>
-          <div>
-            <label className="block text-sm font-medium">Options:</label>
-            <div className="grid grid-cols-2 gap-2 mt-2">
-              {options.map((option, index) => (
-                <input
-                  key={index}
-                  type="text"
-                  value={option}
-                  onChange={(e) => {
-                    const updatedOptions = [...options];
-                    updatedOptions[index] = e.target.value;
-                    setOptions(updatedOptions);
-                  }}
-                  placeholder={`option ${index + 1}`}
-                  className="w-full border rounded-md p-2 mt-2 input input-bordered"
-                  required
-                />
-              ))}
+          <div className="flex flex-wrap -mx-3 mb-6">
+          <div className="w-full md:w-1/2 px-3 mb-6 md:mb-0">
+            <label className="block text-sm font-medium mb-2">
+              আপনার পাঠগুলো এড করুনঃ 
+            </label>
+            <input
+              className="appearance-none block w-full border  border-gray-300 rounded py-3 px-4 mb-3 leading-tight focus:outline-none focus:bg-white"
+              type="text"
+              value={lession}
+              onChange={(e) => setLession(e.target.value)}
+              placeholder="আপনার পাঠ কী?"
+              required
+            />
+          </div>
+          <div className="w-full md:w-1/2 px-3">
+            <label className="block text-sm font-medium mb-2">
+              আপনার পয়েন্টস এড করুনঃ
+            </label>
+            <input
+              className="appearance-none block w-full border border-gray-300 rounded py-3 px-4 leading-tight focus:outline-none focus:bg-white focus:border-gray-500"
+              type="text"
+              value={point}
+              onChange={(e) => setPoint(e.target.value)}
+              placeholder="আপনার পয়েন্ট কত?"
+              required
+            />
+          </div>
+          </div>
+          <div className="w-full md:w-1/2">
+              <label className="block text-sm font-medium mb-2">
+                আপনার কাজের অগ্রগতি কতটুকু?
+              </label>
+              <input
+                className="appearance-none block w-full border border-gray-300 rounded py-3 px-4 leading-tight focus:outline-none focus:bg-white focus:border-gray-500"
+                type="text"
+                value={percent}
+                onChange={(e) => setPercent(e.target.value)}
+                placeholder="আপনার কাজের অগ্রগতি কত পার্সেন্ট?"
+                required
+              />
             </div>
-          </div>
-          <div>
-            <label className="block text-sm font-medium">Correct Answer:</label>
-            <input
-              type="text"
-              value={correctAnswer}
-              onChange={(e) => setCorrectAnswer(e.target.value)}
-              className="w-full border rounded-md p-2 mt-2 input input-bordered"
-              placeholder="correct answer"
-              required
-            />
-          </div>
           <button
-            type="submit"
+            type="submit" 
             className="btn bg-[#088395] hover:bg-[#21bbd3] text-white w-full px-4 py-2 rounded-md font-semibold"
           >
-            Done
+            সম্পন্ন
           </button>
         </form>
       </div>
