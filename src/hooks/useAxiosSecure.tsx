b77fabb904a464c648c4e63b4d1dc211e1fafd43
abcmehedi5 <abcmehedi5@gmail.com>
2023-08-22 13:00:39 +0600

    axios interceptors added

---
+++ src/hooks/useAxiosSecure.tsx
@@ -0,0 +1,41 @@
+import axios from "axios";
+import { useContext, useEffect } from "react";
+import { AuthContext } from "../Providers/AuthProvider";
+import { useNavigate } from "react-router-dom";
+
+const axiosSecure = axios.create({
+  baseURL: "localhost:5000",
+});
+
+const useAxiosSecure = () => {
+  const { logOut }: any = useContext(AuthContext);
+  const navigate = useNavigate();
+
+  useEffect(() => {
+    // request ------------------
+    axiosSecure.interceptors.request.use((config) => {
+      const token = localStorage.getItem("access-token");
+      if (token) {
+        config.headers.Authorization = `Bearer ${token}`;
+      }
+      return config;
+    });
+
+    //  response
+    axiosSecure.interceptors.response.use(
+      (response) => response,
+      async (error) => {
+        if (
+          (error.response && error.response.status === 4001) ||
+          error.response.status === 403
+        ) {
+          await logOut();
+          navigate("/login");
+        }
+        return Promise.reject(error);
+      }
+    );
+  }, [logOut, navigate]);
+  return "somthing";
+};
+export default useAxiosSecure;
