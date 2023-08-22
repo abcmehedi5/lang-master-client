c01135248628e09724f369f5ca590ad026651ea7
abcmehedi5 <abcmehedi5@gmail.com>
2023-08-22 13:05:24 +0600

    base url change

--- src/hooks/useAxiosSecure.tsx
+++ src/hooks/useAxiosSecure.tsx
@@ -4,7 +4,7 @@ import { AuthContext } from "../Providers/AuthProvider";
 import { useNavigate } from "react-router-dom";
 
 const axiosSecure = axios.create({
-  baseURL: "localhost:5000",
+  baseURL: "http://localhost:5000",
 });
 
 const useAxiosSecure = () => {
@@ -36,6 +36,6 @@ const useAxiosSecure = () => {
       }
     );
   }, [logOut, navigate]);
-  return "somthing";
+  return [axiosSecure];
 };
 export default useAxiosSecure;
