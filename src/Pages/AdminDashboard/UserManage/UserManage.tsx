798a330c53fbf313b04136673755350285c8af9f
abcmehedi5 <abcmehedi5@gmail.com>
2023-08-25 17:43:33 +0600

    user bugs fixed

--- src/Pages/AdminDashboard/UserManage/UserManage.tsx
+++ src/Pages/AdminDashboard/UserManage/UserManage.tsx
@@ -1,5 +1,15 @@
+import Chart from "./Chart";
+import UserInfo from "./UserInfo";
+import UserTable from "./UserTable";
+
 const UserManage = () => {
-  return <div></div>;
+  return (
+    <div>
+    <UserInfo></UserInfo>
+    <Chart></Chart>
+    <UserTable></UserTable>
+  </div>
+  );
 };
 
 export default UserManage;
