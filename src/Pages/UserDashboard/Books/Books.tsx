b0b5c61a0f638ef8ed4721a0aeb5d2ec8aa1229b
Akas_Datta <012akasdatta@gmail.com>
2023-08-30 20:51:39 +0600

    added modal and modified json data

--- src/Pages/UserDashboard/Books/Books.tsx
+++ src/Pages/UserDashboard/Books/Books.tsx
@@ -1,4 +1,5 @@
 import React, { useEffect, useState } from "react";
+import { AiFillCloseCircle } from "react-icons/ai";
 import { FaRegStar, FaStar } from "react-icons/fa";
 import Rating from "react-rating";
 
@@ -9,6 +10,7 @@ type Book = {
   writer: string;
   price: number;
   rating: number;
+  description: string;
 };
 
 const Books: React.FC = () => {
@@ -22,12 +24,16 @@ const Books: React.FC = () => {
   };
 
   const handleModalClose = () => {
+    setSelectedBook(null); // Clear the selected book
     setIsModalOpen(false);
   };
 
-  const handleModalSubmit = (book: Book) => {
-    // Handle adding the selected book to the cart
-    console.log("Adding to cart:", book);
+  const handleModalSubmit = () => {
+    // Handle submitting changes or adding to cart
+    if (selectedBook) {
+      console.log("Saving changes:", selectedBook);
+      // You can add more logic here to handle saving changes or adding to cart
+    }
     setIsModalOpen(false);
   };
 
@@ -70,7 +76,10 @@ const Books: React.FC = () => {
             <p className="mt-1.5 text-sm text-gray-700">${book.price}</p>
             <form className="mt-4">
               <button
-                onClick={() => handleEditButtonClick(book)}
+                onClick={(e) => {
+                  e.preventDefault();
+                  handleEditButtonClick(book);
+                }}
                 className="block w-full rounded bg-yellow-400 p-4 text-sm font-medium transition hover:scale-105"
               >
                 Add to Cart
@@ -80,31 +89,62 @@ const Books: React.FC = () => {
         </div>
       ))}
       {isModalOpen && selectedBook && (
+        <div className="fixed inset-0 flex items-center justify-center z-50">
+          <div className="fixed inset-0 bg-black opacity-70"></div>
+          <div className="bg-white p-6 rounded-lg shadow-md relative md:w-1/2">
+            <div className="flex justify-end">
+              <div
+                className="px-2 pb-3 text-yellow-500 text-5xl cursor-pointer"
+                onClick={handleModalClose}
+              >
+                <AiFillCloseCircle></AiFillCloseCircle>
+              </div>
+            </div>
+            <div>
 
+              <div className="group block">
+                <img
+                  src={selectedBook.bookimage}
+                  alt=""
+                  className="h-[350px] w-full object-cover sm:h-[450px]"
+                />
 
-          <div className="">
-            <img
-              src={selectedBook.bookimage}
-              alt=""
-              className="h-[350px] w-full object-cover sm:h-[450px]"
-            />
+                <div className="mt-3 flex justify-between text-sm">
+                  <div>
+                    <h3
+                      className="text-gray-900 group-hover:underline group-hover:underline-offset-4 font-bold text-xl"
+                    >
+                      {selectedBook.bookname}
+                    </h3>
 
-            <div className="mt-3 flex justify-between text-sm">
-              <div>
-                <h3
-                  className="text-gray-900 group-hover:underline group-hover:underline-offset-4"
-                >
-                  {selectedBook.bookname}
-                </h3>
+                    <p className="mt-1.5 max-w-[45ch] text-xs text-gray-700">
+                      {selectedBook.description}
+                    </p>
+                  </div>
 
-                <p className="mt-1.5 max-w-[45ch] text-xs text-gray-500">
-                {selectedBook.descriptions}
-                </p>
+                  <p className="text-gray-900">${selectedBook.price}</p>
+                </div>
               </div>
 
-              <p className="text-gray-900">{selectedBook.price}</p>
+
+
+              <div className="flex justify-end mt-4">
+                <button
+                  onClick={handleModalSubmit}
+                  className="px-4 py-2 bg-yellow-400 text-black rounded"
+                >
+                  Buy Now
+                </button>
+                <button
+                  onClick={handleModalClose}
+                  className="px-4 py-2 ml-4 border rounded"
+                >
+                  Cancel
+                </button>
+              </div>
             </div>
           </div>
+        </div>
       )}
     </div>
   );
