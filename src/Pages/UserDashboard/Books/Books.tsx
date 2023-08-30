672ac863352ad9d7cbe73009ec67690a976ce784
Akas_Datta <012akasdatta@gmail.com>
2023-08-30 19:09:06 +0600

    book is modified

--- src/Pages/UserDashboard/Books/Books.tsx
+++ src/Pages/UserDashboard/Books/Books.tsx
@@ -1,7 +1,6 @@
 import React, { useEffect, useState } from "react";
 import { FaRegStar, FaStar } from "react-icons/fa";
 import Rating from "react-rating";
-import CartModal from "./CartModal";
 
 type Book = {
   id: number;
@@ -15,8 +14,10 @@ type Book = {
 const Books: React.FC = () => {
   const [books, setBooks] = useState<Book[]>([]);
   const [isModalOpen, setIsModalOpen] = useState(false);
+  const [selectedBook, setSelectedBook] = useState<Book | null>(null);
 
-  const handleEditButtonClick = () => {
+  const handleEditButtonClick = (book: Book) => {
+    setSelectedBook(book);
     setIsModalOpen(true);
   };
 
@@ -24,13 +25,14 @@ const Books: React.FC = () => {
     setIsModalOpen(false);
   };
 
-  const handleModalSubmit = (data: Profile) => {
-    // Handle the data submitted from the modal (e.g., update the state or send to server)
-    console.log(data);
+  const handleModalSubmit = (book: Book) => {
+    // Handle adding the selected book to the cart
+    console.log("Adding to cart:", book);
+    setIsModalOpen(false);
   };
 
   useEffect(() => {
-    fetch("/books.json") // Make sure the path is correct
+    fetch("/books.json")
       .then((res) => res.json())
       .then((data) => setBooks(data))
       .catch((error) => console.error(error));
@@ -39,30 +41,20 @@ const Books: React.FC = () => {
   return (
     <div className="grid grid-cols-1 gap-4 md:grid-cols-3 lg:grid-cols-4 p-14">
       {books.map((book) => (
-        <p
+        <div
           key={book.id}
-          href="#"
           className="group relative block overflow-hidden cursor-pointer"
         >
-          <button className="absolute end-4 top-4 z-10 rounded-full bg-white p-1.5 text-gray-900 transition hover:text-gray-900/75">
-            <span className="sr-only">Wishlist</span>
-            {/* ... (wishlist icon SVG) */}
-          </button>
           <img
             src={book.bookimage}
             alt=""
             className="h-64 w-full object-cover transition duration-500 group-hover:scale-105 sm:h-72"
           />
           <div className="relative border border-gray-100 bg-white p-6">
-            <span className="whitespace-nowrap bg-yellow-400 px-3 py-1.5 text-xs font-medium">
-              {book.role}
-            </span>
             <h3 className="mt-4 text-lg font-medium text-gray-900">
               {book.bookname}
             </h3>
-            <h3 className="mt-4 font-medium">
-              {book.writer}
-            </h3>
+            <h3 className="mt-4 font-medium">{book.writer}</h3>
             <div className="flex items-center">
               <p>
                 <Rating
@@ -77,18 +69,43 @@ const Books: React.FC = () => {
             </div>
             <p className="mt-1.5 text-sm text-gray-700">${book.price}</p>
             <form className="mt-4">
-              <button  onClick={handleEditButtonClick} className="block w-full rounded bg-yellow-400 p-4 text-sm font-medium transition hover:scale-105">
+              <button
+                onClick={() => handleEditButtonClick(book)}
+                className="block w-full rounded bg-yellow-400 p-4 text-sm font-medium transition hover:scale-105"
+              >
                 Add to Cart
               </button>
             </form>
           </div>
-        </p>
+        </div>
       ))}
-        <CartModal
-        isOpen={isModalOpen}
-        onClose={handleModalClose}
-        onSubmit={handleModalSubmit}
-      />
+      {isModalOpen && selectedBook && (
+
+
+          <div className="">
+            <img
+              src={selectedBook.bookimage}
+              alt=""
+              className="h-[350px] w-full object-cover sm:h-[450px]"
+            />
+
+            <div className="mt-3 flex justify-between text-sm">
+              <div>
+                <h3
+                  className="text-gray-900 group-hover:underline group-hover:underline-offset-4"
+                >
+                  {selectedBook.bookname}
+                </h3>
+
+                <p className="mt-1.5 max-w-[45ch] text-xs text-gray-500">
+                {selectedBook.descriptions}
+                </p>
+              </div>
+
+              <p className="text-gray-900">{selectedBook.price}</p>
+            </div>
+          </div>
+      )}
     </div>
   );
 };
--- src/Pages/UserDashboard/Books/CartModal.tsx
+++ src/Pages/UserDashboard/Books/CartModal.tsx
@@ -1,46 +1,30 @@
-import React, { useState } from "react";
+// CartModal.tsx
+import React from "react";
 
-interface ProfileData {
-  address: string;
-  location: string;
-  site: string;
-  birthday: string;
-  gender: string;
-  genderLevel: string;
+interface Book {
+  id: number;
+  bookname: string;
+  bookimage: string;
+  writer: string;
+  price: number;
+  rating: number;
 }
 
-interface ProfileEditModalProps {
+interface CartModalProps {
   isOpen: boolean;
   onClose: () => void;
-  onSubmit: (data: ProfileData) => void;
+  selectedBook: Book | null;
+  onSubmit: (book: Book) => void;
 }
 
-const ProfileEditModal: React.FC<ProfileEditModalProps> = ({
-  isOpen,
-  onClose,
-  onSubmit,
-}) => {
-  const [address, setAddress] = useState("");
-  const [location, setLocation] = useState("");
-  const [site, setSite] = useState("");
-  const [birthday, setBirthday] = useState("");
-  const [gender, setGender] = useState("");
-  const [genderLevel, setGenderLevel] = useState("");
-
+const CartModal: React.FC<CartModalProps> = ({ isOpen, onClose, selectedBook, onSubmit }) => {
   const handleSubmit = () => {
-    const data: ProfileData = {
-      address,
-      location,
-      site,
-      birthday,
-      gender,
-      genderLevel,
-    };
-    onSubmit(data);
-    onClose();
+    if (selectedBook) {
+      onSubmit(selectedBook);
+    }
   };
 
-  if (!isOpen) {
+  if (!isOpen || !selectedBook) {
     return null;
   }
 
@@ -48,34 +32,11 @@ const ProfileEditModal: React.FC<ProfileEditModalProps> = ({
     <div className="fixed inset-0 flex items-center justify-center z-50">
       <div className="fixed inset-0 bg-black opacity-70"></div>
       <div className="bg-white p-6 rounded-lg shadow-md relative md:w-1/2">
-        <h2 className="text-lg font-semibold mb-4">Update Profile Info</h2>
+        <h2 className="text-lg font-semibold mb-4">Add to Cart</h2>
         <div className="space-y-4">
- 
-
-        <a href="#" className="group block">
-            <img
-                src="https://images.unsplash.com/photo-1592921870789-04563d55041c?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=774&q=80"
-                alt=""
-                className="h-[350px] w-full object-cover sm:h-[450px]"
-            />
-
-            <div className="mt-3 flex justify-between text-sm">
-                <div>
-                <h3
-                    className="text-gray-900 group-hover:underline group-hover:underline-offset-4"
-                >
-                    Small Headphones
-                </h3>
-
-                <p className="mt-1.5 max-w-[45ch] text-xs text-gray-500">
-                    Lorem ipsum dolor sit amet consectetur adipisicing elit. Quasi nobis,
-                    quia soluta quisquam voluptatem nemo.
-                </p>
-                </div>
-
-                <p className="text-gray-900">$299</p>
-            </div>
-            </a>
+          <h3 className="text-gray-900">{selectedBook.bookname}</h3>
+          <p className="text-xs text-gray-500">${selectedBook.price}</p>
+          {/* ... (other book details) */}
         </div>
         <div className="flex justify-end mt-4">
           <button
@@ -93,4 +54,4 @@ const ProfileEditModal: React.FC<ProfileEditModalProps> = ({
   );
 };
 
-export default ProfileEditModal;
+export default CartModal;
