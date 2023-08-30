41276371575c61b7be17032244a5cafd7cb048d1
Akas_Datta <012akasdatta@gmail.com>
2023-08-30 11:39:40 +0600

    books component done

--- src/Pages/UserDashboard/Books/Books.tsx
+++ src/Pages/UserDashboard/Books/Books.tsx
@@ -1,4 +1,6 @@
 import React, { useEffect, useState } from "react";
+import { FaRegStar, FaStar } from "react-icons/fa";
+import Rating from "react-rating";
 
 type Book = {
   id: number;
@@ -20,7 +22,7 @@ const Books: React.FC = () => {
   }, []);
 
   return (
-    <div className="grid grid-cols-2 gap-4 md:grid-cols-3 lg:grid-cols-4">
+    <div className="grid grid-cols-1 gap-4 md:grid-cols-3 lg:grid-cols-4 p-14">
       {books.map((book) => (
         <a
           key={book.id}
@@ -29,20 +31,7 @@ const Books: React.FC = () => {
         >
           <button className="absolute end-4 top-4 z-10 rounded-full bg-white p-1.5 text-gray-900 transition hover:text-gray-900/75">
             <span className="sr-only">Wishlist</span>
-            <svg
-              xmlns="http://www.w3.org/2000/svg"
-              fill="none"
-              viewBox="0 0 24 24"
-              strokeWidth="1.5"
-              stroke="currentColor"
-              className="h-4 w-4"
-            >
-              <path
-                strokeLinecap="round"
-                strokeLinejoin="round"
-                d="M21 8.25c0-2.485-2.099-4.5-4.688-4.5-1.935 0-3.597 1.126-4.312 2.733-.715-1.607-2.377-2.733-4.313-2.733C5.1 3.75 3 5.765 3 8.25c0 7.22 9 12 9 12s9-4.78 9-12z"
-              />
-            </svg>
+            {/* ... (wishlist icon SVG) */}
           </button>
           <img
             src={book.bookimage}
@@ -59,6 +48,18 @@ const Books: React.FC = () => {
             <h3 className="mt-4 font-medium">
               {book.writer}
             </h3>
+            <div className="flex items-center">
+              <p>
+                <Rating
+                  placeholderRating={book.rating}
+                  readonly
+                  emptySymbol={<FaRegStar />}
+                  placeholderSymbol={<FaStar className="text-yellow-500" />}
+                  fullSymbol={<FaStar />}
+                />{" "}
+                {book.rating}
+              </p>
+            </div>
             <p className="mt-1.5 text-sm text-gray-700">${book.price}</p>
             <form className="mt-4">
               <button className="block w-full rounded bg-yellow-400 p-4 text-sm font-medium transition hover:scale-105">
