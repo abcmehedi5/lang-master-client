6f0a13b5917c98db6d6e0cd3e552c5bf82c67191
ArjumanJesmin <arjumanjesmin221186@gmail.com>
2023-09-03 23:42:25 +0600

    complete payment UI

--- src/Pages/UserDashboard/Shop/CoinBuy.tsx
+++ src/Pages/UserDashboard/Shop/CoinBuy.tsx
@@ -1,5 +1,7 @@
 import { useState } from "react";
 import BuyingAmount from "./BuyingAmount";
+import Lottie from "lottie-react-web";
+import payCoin from "../../../../public/payCoin.json"
 const CoinBuy = () => {
   const [coinAmount, setCoinAmount] = useState<number>(0);
   const [isModalOpen, setIsModalOpen] = useState(false);
@@ -18,14 +20,13 @@ const CoinBuy = () => {
 
   const totalAmountToBePaid = (coinAmount * 0.1).toFixed(2);
   return (
-    <div className="grid grid-cols-1 md:grid-cols-12 gap-24 w-9/12 mx-auto mt-16">
+    <div className="grid grid-cols-1 md:grid-cols-12 gap-2 w-11/12 mx-auto mt-16 border m-4 p-4 rounded-lg shadow-lg border-amber-400">
       {/* left side  */}
-      <div className="col-span-4">
+      <div className="col-span-4 ">
         <div>
-          <h1 className="text-3xl font-bold">কয়েন কিনুন</h1>
+          <h1 className="text-3xl font-bold text-amber-500">কয়েন কিনুন</h1>
           <hr className="mt-4 w-[50%]" />
         </div>
-
         <div className="my-5">
           <label>
             <span className="block mb-3">কত কয়েন কিনতে চান তা লিখুনঃ</span>
@@ -33,7 +34,7 @@ const CoinBuy = () => {
               type="number"
               placeholder="এখানে লিখুন"
               onChange={handleCoinAmountChange}
-              className="input input-bordered input-info w-full max-w-xs"
+              className="input input-bordered input-primary w-full max-w-xs"
               required
             />
             <button
@@ -61,18 +62,35 @@ const CoinBuy = () => {
               </div>
             )}
           </>
+          <div className="pt-14">
+            <h3 className="font-semibold text-base">কয়েন দিয়ে কি করতে পারবে?</h3>
+            <p className="max-w-xl font-thin text-slate-700">
+              কয়েন ব্যবহার করে একজন ইউজার আমাদের পিডিএফ বই অথবা প্রয়োজনীয় ডকুমেন্ট
+              কিনতে পারবে।  সেইসাথে নতুন ইউনিট আনলক করার ক্ষেত্রেও কয়েন কাজে
+              আসবে।
+            </p>
+          </div>
         </div>
       </div>
 
       {/* RIght side  */}
       <div className="col-span-8">
-        <h1 className="font-bold text-xl">কয়েন দিয়ে কি করতে পারবে?</h1>
-        <p className="max-w-xl">
+        {/* <h1 className="font-bold text-xl">কয়েন দিয়ে কি করতে পারবে?</h1> */}
+        {/* <p className="max-w-xl">
           কয়েন ব্যবহার করে একজন ইউজার আমাদের পিডিএফ বই অথবা প্রয়োজনীয় ডকুমেন্ট
           কিনতে পারবে। <br /> সেইসাথে নতুন ইউনিট আনলক করার ক্ষেত্রেও কয়েন কাজে
           আসবে।
-        </p>
+        </p> */}
+        <Lottie
+          options={{
+            animationData: payCoin,
+            loop: true,
+            autoplay: true,
+          }}
+          height={500}
+        />
       </div>
+
     </div>
   );
 };
