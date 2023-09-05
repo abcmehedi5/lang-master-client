26d03a60d257f42376df9a2116b29323c50ac9cd
Akas_Datta <012akasdatta@gmail.com>
2023-09-05 17:57:06 +0600

    completed email functionality

--- src/Pages/UserDashboard/Shop/CoinBuy.tsx
+++ src/Pages/UserDashboard/Shop/CoinBuy.tsx
@@ -1,7 +1,10 @@
-import { useState } from "react";
+import { useRef, useState } from "react";
 import BuyingAmount from "./BuyingAmount";
 import Lottie from "lottie-react-web";
-import payCoin from "../../../../public/payCoin.json"
+import payCoin from "../../../../public/payCoin.json";
+import emailjs from '@emailjs/browser';
+
+
 const CoinBuy = () => {
   const [coinAmount, setCoinAmount] = useState<number>(0);
   const [isModalOpen, setIsModalOpen] = useState(false);
@@ -18,6 +21,22 @@ const CoinBuy = () => {
     setCoinAmount(Number(event.target.value));
   };
 
+  const form = useRef();
+  const sendEmail = (e) => {
+    e.preventDefault();
+
+    emailjs.sendForm(
+      'service_mpetxyt', 
+      'template_fw43tmi', 
+      form.current, 
+      'bZAyBv6M_AfAxwGJW')
+      .then((result) => {
+          console.log(result.text);
+      }, (error) => {
+          console.log(error.text);
+      });
+  };
+
   const totalAmountToBePaid = (coinAmount * 0.1).toFixed(2);
   return (
     <div className="grid grid-cols-1 md:grid-cols-12 gap-2 w-11/12 mx-auto mt-16 border m-4 p-4 rounded-lg shadow-lg border-amber-400">
@@ -28,23 +47,51 @@ const CoinBuy = () => {
           <hr className="mt-4 w-[50%]" />
         </div>
         <div className="my-5">
-          <label>
-            <span className="block mb-3">কত কয়েন কিনতে চান তা লিখুনঃ</span>
-            <input
-              type="number"
-              placeholder="এখানে লিখুন"
-              onChange={handleCoinAmountChange}
-              className="input input-bordered input-primary w-full max-w-xs"
-              required
-            />
-            <button
-              onClick={openModal}
-              className="defaultBtn px-4 py-8 block mt-4"
-              disabled={!coinAmount}
-            >
-              Buy now
-            </button>
-          </label>
+          <form ref={form} onSubmit={sendEmail}>
+            <label>
+              <span className="block mb-3">আপনার নাম লিখুনঃ</span>
+              <input
+                type="text"
+                placeholder="এখানে লিখুন"
+                name="name"
+                className="input input-bordered input-primary w-full max-w-xs"
+                required
+              /> 
+              <span className="block mb-3">আপনার ইমেইল দিনঃ</span>
+              <input
+                type="email"
+                placeholder="এখানে লিখুন"
+                name="email"
+                className="input input-bordered input-primary w-full max-w-xs"
+                required
+              /> 
+              <span className="block my-3">আপনার মতামত দিনঃ</span>
+              <textarea
+                type="text"
+                placeholder="এখানে লিখুন"
+                rows="4" cols="50"
+                name="message"
+                className="input pt-3 input-bordered input-primary w-full max-w-xs"
+                required
+              />
+              <span className="block my-3">কত কয়েন কিনতে চান তা লিখুনঃ</span>
+              <input
+                type="number"
+                placeholder="এখানে লিখুন"
+                name="coin"
+                onChange={handleCoinAmountChange}
+                className="input input-bordered input-primary w-full max-w-xs"
+                required
+              />
+              <button
+                onClick={openModal}
+                className="defaultBtn px-4 py-8 block mt-4"
+                disabled={!coinAmount}
+              >
+                Buy now
+              </button>
+            </label>
+          </form>
           <>
             {isModalOpen && (
               <div className="fixed inset-0 z-40 flex items-center justify-center">
