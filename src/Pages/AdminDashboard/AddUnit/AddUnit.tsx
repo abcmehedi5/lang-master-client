a48c5d614b412bbbbd224796941ed16d1a90445c
Jhimi Talukder <talukderjhimi@gmail.com>
2023-09-07 13:27:10 +0600

    addunitdata

--- src/Pages/AdminDashboard/AddUnit/AddUnit.tsx
+++ src/Pages/AdminDashboard/AddUnit/AddUnit.tsx
@@ -1,134 +1,165 @@
 import { useState } from "react";
-import useAxiosSecure from "../../../hooks/useAxiosSecure";
-const AddUnit = () => {
-  const [unit, setUnit] = useState({});
-  const [lesson, setLesson] = useState({});
-  const [quiz, setQuiz] = useState([]);
-  const [axiosSecure] = useAxiosSecure();
-  // unit data
-  const handleUnitChange = (e: any) => {
-    const newUnit = { ...unit };
-    newUnit[e.target.name] = e.target.value;
-    setUnit(newUnit);
-  };
 
-  // lesson data
+function AddUnit() {
+  const [unit, setUnit] = useState("");
+  const [unitTopic, setUnitTopic] = useState("");
+  const [totalLesson, setTotalLesson] = useState("");
+  const [lessons, setLessons] = useState([
+    {
+      lessonNumber: "",
+      lessonTitle: "",
+      points: "",
+      quiz: [
+        {
+          question: "",
+          options: ["", "", "", ""],
+          correctAnswer: "",
+        },
+      ],
+    },
+  ]);
 
-  const handleLessonChange = (e: any) => {
-    const newLesson = { ...lesson };
-    newLesson[e.target.name] = e.target.value;
-    setLesson(newLesson);
-  };
-
-  // lessons data
-  const handleQuizChange = (e: any) => {
-    const newquiz = { ...quiz };
-    newquiz[e.target.name] = e.target.value;
-    setQuiz(newquiz);
-  };
-
-  const handleSubmit = async (e: any) => {
+  const handleSubmit = (e) => {
     e.preventDefault();
-    const res = await axiosSecure.post("/unitMake/createUnit", {
-      unit: unit,
-      lessons: [lesson],
-      quiz: [quiz],
-    });
-    console.log(res);
+    e.preventDefault();
+    const formData = {
+      unit,
+      topic: unitTopic,
+      totalLesson,
+      lessons,
+    };
+    console.log(formData);
   };
 
-  const add = {
-    unit: unit,
-    lessons: [lesson],
-    quiz: [quiz],
-  }
+  const handleLessonChange = (e, lessonIndex, field) => {
+    const newLessons = [...lessons];
+    newLessons[lessonIndex][field] = e.target.value;
+    setLessons(newLessons);
+  };
 
-  console.log(add);
-  
+  const handleQuizChange = (e, lessonIndex, quizIndex, optionIndex) => {
+    const newLessons = [...lessons];
+    newLessons[lessonIndex].quiz[quizIndex].options[optionIndex] =
+      e.target.value;
+    setLessons(newLessons);
+  };
+
+  const handleQuizQuestionChange = (e, lessonIndex, quizIndex) => {
+    const newLessons = [...lessons];
+    newLessons[lessonIndex].quiz[quizIndex].question = e.target.value;
+    setLessons(newLessons);
+  };
+
+  const handleCorrectAnswerChange = (e, lessonIndex, quizIndex) => {
+    const newLessons = [...lessons];
+    newLessons[lessonIndex].quiz[quizIndex].correctAnswer = e.target.value;
+    setLessons(newLessons);
+  };
 
   return (
-    <div className="container">
-      <form onSubmit={handleSubmit}>
-        <label>Unit</label>
+    <form onSubmit={handleSubmit}>
+      <div>
         <input
-          className="w-full border rounded-md p-2 mt-2 input input-bordered"
           name="unit"
-          onChange={handleUnitChange}
+          placeholder="Unit Name"
+          className="input input-bordered w-full max-w-xs"
+          value={unit}
+          onChange={(e) => setUnit(e.target.value)}
         />
-
-        <label>topic</label>
         <input
-          className="w-full border rounded-md p-2 mt-2 input input-bordered"
           name="topic"
-          onChange={handleUnitChange}
+          placeholder="Unit Topic"
+          className="input input-bordered w-full max-w-xs"
+          value={unitTopic}
+          onChange={(e) => setUnitTopic(e.target.value)}
         />
-
-        <label>Total Lessons</label>
         <input
-          className="w-full border rounded-md p-2 mt-2 input input-bordered"
           name="totalLessons"
-          onChange={handleUnitChange}
+          type="number"
+          placeholder="Total Lessons"
+          className="input input-bordered w-full max-w-xs"
+          value={totalLesson}
+          onChange={(e) => setTotalLesson(e.target.value)}
         />
+      </div>
 
-        <label>lesson Number</label>
-        <input
-          className="w-full border rounded-md p-2 mt-2 input input-bordered"
-          name="lessonNumber"
-          onChange={handleLessonChange}
-        />
+      {lessons.map((lesson, lessonIndex) => (
+        <div key={lessonIndex} className="mt-4 border p-4">
+          <input
+            name="lessonNumber"
+            type="text"
+            placeholder="Lesson Number"
+            className="input input-bordered w-full max-w-xs"
+            value={lesson.lessonNumber}
+            onChange={(e) => handleLessonChange(e, lessonIndex, "lessonNumber")}
+          />
+          <input
+            name="lessonTitle"
+            type="text"
+            placeholder="Lesson Title"
+            className="input input-bordered w-full max-w-xs"
+            value={lesson.lessonTitle}
+            onChange={(e) => handleLessonChange(e, lessonIndex, "lessonTitle")}
+          />
+          <input
+            name="points"
+            type="number"
+            placeholder="Points"
+            className="input input-bordered w-full max-w-xs"
+            value={lesson.points}
+            onChange={(e) => handleLessonChange(e, lessonIndex, "points")}
+          />
 
-        <label>lesson Title</label>
-        <input
-          className="w-full border rounded-md p-2 mt-2 input input-bordered"
-          name="lessonTitle"
-          onChange={handleLessonChange}
-        />
-        <label>Points</label>
-        <input
-          className="w-full border rounded-md p-2 mt-2 input input-bordered"
-          name="points"
-          onChange={handleLessonChange}
-        />
+          {lesson.quiz.map((quiz, quizIndex) => (
+            <div key={quizIndex} className="mt-2">
+              <input
+                name="question"
+                type="text"
+                placeholder="Question"
+                className="input input-bordered w-full max-w-xs"
+                value={quiz.question}
+                onChange={(e) =>
+                  handleQuizQuestionChange(e, lessonIndex, quizIndex)
+                }
+              />
 
-        <label>Question</label>
-        <input
-          className="w-full border rounded-md p-2 mt-2 input input-bordered"
-          name="question"
-          onChange={handleQuizChange}
-        />
+              {quiz.options.map((option, optionIndex) => (
+                <input
+                  key={optionIndex}
+                  name="options"
+                  type="text"
+                  placeholder={`Option ${optionIndex + 1}`}
+                  className="input input-bordered w-full max-w-xs"
+                  value={option}
+                  onChange={(e) =>
+                    handleQuizChange(e, lessonIndex, quizIndex, optionIndex)
+                  }
+                />
+              ))}
 
-        <label>Options</label>
-        <input
-          className="w-full border rounded-md p-2 mt-2 input input-bordered"
-          name="options"
-        />
-        <label>Options</label>
-        <input
-          className="w-full border rounded-md p-2 mt-2 input input-bordered"
-          name="options"
-        />
-        <label>Options</label>
-        <input
-          className="w-full border rounded-md p-2 mt-2 input input-bordered"
-          name="options"
-        />
-        <label>Options</label>
-        <input
-          className="w-full border rounded-md p-2 mt-2 input input-bordered"
-          name="options"
-        />
+              <input
+                name="correctAnswer"
+                type="text"
+                placeholder="Correct Answer"
+                className="input input-bordered w-full max-w-xs"
+                value={quiz.correctAnswer}
+                onChange={(e) =>
+                  handleCorrectAnswerChange(e, lessonIndex, quizIndex)
+                }
+              />
+            </div>
+          ))}
+        </div>
+      ))}
 
-        <label>Correct Answer</label>
-        <input
-          className="w-full border rounded-md p-2 mt-2 input input-bordered"
-          name="correctAnswer"
-          onChange={handleQuizChange}
-        />
-
-        <input className="defaultBtn" type="submit" />
-      </form>
-    </div>
+      <button
+        type="submit"
+        className="mt-4 bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded"
+      >
+        Submit
+      </button>
+    </form>
   );
-};
+}
 
 export default AddUnit;
