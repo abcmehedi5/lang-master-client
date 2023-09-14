64c3c1887eec13c82f2555ba09b5d5d4c24a0fdf
Akas_Datta <012akasdatta@gmail.com>
2023-09-15 00:28:53 +0600

    terms and condition complete

--- src/Pages/TermsAndCondition/TermsAndCondition.tsx
+++ src/Pages/TermsAndCondition/TermsAndCondition.tsx
@@ -1,84 +1,67 @@
 const TermsAndCondition = () => {
     return (
-        <div>
-            <h1 className="font-bold text-3xl m-10">Terms and Conditions of Service</h1>
-            <p>
-            Langmaster - Terms and Conditions
+        <div className="lg:mx-64 md:px-24 px-12">
+            <h1 className="font-bold text-4xl my-10">Terms and Conditions of Service    
+            <hr className="border-1 border-black mt-2"/></h1>
+            <div className="text-xl">
+                <p className="mb-5 font-bold text-xl">Welcome to Langmaster! These terms and conditions ("Terms") govern your use of the Langmaster website and mobile applications (collectively, "the Service"). By using Langmaster, you agree to be bound by these Terms. If you do not agree to abide by these Terms and Conditions, you are not authorized to use, access, or participate in the Service.</p> <br />
 
-Welcome to Langmaster! These terms and conditions ("Terms") govern your use of the Langmaster website and mobile applications (collectively, "the Service"). By using Langmaster, you agree to be bound by these Terms. If you do not agree with these Terms, please do not use the Service.
+                <p className="font-bold text-2xl my-3">1. Definitions</p>
 
-1. Definitions
+                "Langmaster": Refers to EndGame Company, the provider of the language learning platform.
+                "User": Refers to any individual accessing or using the Service.
+                <p className="font-bold text-2xl my-3 pt-4">   2. Acceptance of Terms</p>
 
-1.1 "Langmaster": Refers to [Your Company], the provider of the language learning platform.
+                Eligibility: You must be at least 13 years old to use the Service. If you are under 13, you may only use the Service under the supervision of a parent or legal guardian.
+                Account Information: You are responsible for maintaining the confidentiality of your account information, including your password. You agree to notify us immediately of any unauthorized use of your account.
+                <p className="font-bold text-2xl my-3 pt-4">   3. User Accounts</p>
 
-1.2 "User": Refers to any individual accessing or using the Service.
+                Registration: In order to use certain features of the Service, you may be required to register for an account. By doing so, you agree to provide accurate and complete information.
+                Account Security: You are responsible for maintaining the security of your account and are prohibited from sharing your login credentials with any third party.
+                <p className="font-bold text-2xl my-3 pt-4">4. Content and Intellectual Property</p>
 
-2. Acceptance of Terms
+                Ownership: The Service, including all content, features, and functionality, is owned by Langmaster and protected by intellectual property laws.
+                User-Generated Content: You may submit content to the Service. By doing so, you grant Langmaster a worldwide, non-exclusive, royalty-free license to use, reproduce, modify, adapt, publish, translate, distribute, and display such content.
+                <p className="font-bold text-2xl my-3 pt-4"> 5. Prohibited Activities</p>
 
-2.1 Eligibility: You must be at least 13 years old to use the Service. If you are under 13, you may only use the Service under the supervision of a parent or legal guardian.
+                Violating Laws or Regulations: Using the Service for any purpose that is unlawful or prohibited by these Terms.
+                Harmful Conduct: Engaging in any conduct that could harm Langmaster, other users, or third parties.
+                Data Mining or Harvesting: Attempting to collect data from the Service, including user information, without our prior written consent.
+                <p className="font-bold text-2xl my-3 pt-4">6. Privacy</p>
 
-2.2 Account Information: You are responsible for maintaining the confidentiality of your account information, including your password. You agree to notify us immediately of any unauthorized use of your account.
+                Privacy Policy: Your use of the Service is subject to our Privacy Policy, which is incorporated by reference into these Terms.
+                <p className="font-bold text-2xl my-3 pt-4">7. Termination</p>
 
-3. User Accounts
+                Termination by User: You may terminate your account at any time by contacting us or using the provided account termination tools.
+                Termination by Langmaster: We may, at our sole discretion, suspend or terminate your access to the Service for any reason.
+                <p className="font-bold text-2xl my-3 pt-4">8. Disclaimer of Warranties</p>
+                
 
-3.1 Registration: In order to use certain features of the Service, you may be required to register for an account. By doing so, you agree to provide accurate and complete information.
+                As-Is Basis: The Service is provided "as is" and "as available" without warranties of any kind, whether express or implied.
+                <p className="font-bold text-2xl my-3 pt-4">9. Limitation of Liability</p>
+                
 
-3.2 Account Security: You are responsible for maintaining the security of your account and are prohibited from sharing your login credentials with any third party.
+                No Liability for Consequential Damages: In no event shall Langmaster be liable for any indirect, consequential, special, incidental, or punitive damages.
+                <p className="font-bold text-2xl my-3 pt-4">10. Changes to Terms </p>
+                
+                Modification of Terms: We reserve the right to modify these Terms at any time. Any changes will be effective immediately upon posting.
+                <p className="font-bold text-2xl my-3 pt-4">11. Governing Law and Jurisdiction</p>
+                
 
-4. Content and Intellectual Property
+                Governing Law: These Terms shall be governed by and construed in accordance with the laws of [Dhaka, Bangladesh].
+                Jurisdiction: Any disputes arising under or in connection with these Terms shall be subject to the exclusive jurisdiction of the courts of [Dhaka, Bangladesh].
+                <p className="font-bold text-2xl my-3 pt-4">12. Entire Agreement</p>
+                
 
-4.1 Ownership: The Service, including all content, features, and functionality, is owned by Langmaster and protected by intellectual property laws.
+                Entire Agreement: These Terms constitute the entire agreement between you and Langmaster regarding the use of the Service.
+                <p className="font-bold text-2xl my-3 pt-4">13. Contact Information</p>
+                
 
-4.2 User-Generated Content: You may submit content to the Service. By doing so, you grant Langmaster a worldwide, non-exclusive, royalty-free license to use, reproduce, modify, adapt, publish, translate, distribute, and display such content.
+                Questions or Concerns: If you have any questions or concerns about these Terms, please contact us at <b>langmaster50@gmail.com. </b>
 
-5. Prohibited Activities
-
-5.1 Violating Laws or Regulations: Using the Service for any purpose that is unlawful or prohibited by these Terms.
-
-5.2 Harmful Conduct: Engaging in any conduct that could harm Langmaster, other users, or third parties.
-
-5.3 Data Mining or Harvesting: Attempting to collect data from the Service, including user information, without our prior written consent.
-
-6. Privacy
-
-6.1 Privacy Policy: Your use of the Service is subject to our Privacy Policy, which is incorporated by reference into these Terms.
-
-7. Termination
-
-7.1 Termination by User: You may terminate your account at any time by contacting us or using the provided account termination tools.
-
-7.2 Termination by Langmaster: We may, at our sole discretion, suspend or terminate your access to the Service for any reason.
-
-8. Disclaimer of Warranties
-
-8.1 As-Is Basis: The Service is provided "as is" and "as available" without warranties of any kind, whether express or implied.
-
-9. Limitation of Liability
-
-9.1 No Liability for Consequential Damages: In no event shall Langmaster be liable for any indirect, consequential, special, incidental, or punitive damages.
-
-10. Changes to Terms
-
-10.1 Modification of Terms: We reserve the right to modify these Terms at any time. Any changes will be effective immediately upon posting.
-
-11. Governing Law and Jurisdiction
-
-11.1 Governing Law: These Terms shall be governed by and construed in accordance with the laws of [Your Jurisdiction].
-
-11.2 Jurisdiction: Any disputes arising under or in connection with these Terms shall be subject to the exclusive jurisdiction of the courts of [Your Jurisdiction].
-
-12. Entire Agreement
-
-12.1 Entire Agreement: These Terms constitute the entire agreement between you and Langmaster regarding the use of the Service.
-
-13. Contact Information
-
-13.1 Questions or Concerns: If you have any questions or concerns about these Terms, please contact us at [contact email].
-
-Effective Date: These Terms were last updated on [Date].
-
-Again, please ensure to replace the placeholders with specific information related to your project and consult a legal professional to verify that these terms meet your project's specific needs.
-            </p>
+                Make sure to replace the placeholders with specific information related to your project. Always consult with a legal professional to ensure that these terms are legally sound and appropriate for your specific needs.
+                <p className="font-bold my-16">Last revised on September 15th, 2023</p>
+            </div>
         </div>
     );
 };
